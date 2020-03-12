3bb6f8f09ca22d972b1974bc62c7c08c87ce254b
PhillC <phill@starkingdom.co.uk>
2020-03-12 22:12:58 +0100

    Make me a better person, use specific namespaces for package functions

--- R/duckdf.R
+++ R/duckdf.R
@@ -8,22 +8,22 @@
 duckdf <- function(query = ""){
 
 # Extract dataframe name using sting splits
-query_split <- stri_split_fixed(query, "FROM", omit_empty=TRUE)
+query_split <- stringi::stri_split_fixed(query, "FROM", omit_empty=TRUE)
 query_split <- trimws(unlist(query_split))
 
-from_df <- stri_extract_first_words(query_split[2], locale = NULL)
+from_df <- stringi::stri_extract_first_words(query_split[2], locale = NULL)
 
 # create a DuckDB connection, either as a temporary in-memory database (default) or with a file
-con <- dbConnect(duckdb::duckdb(), ":memory:")
+con <- DBI::dbConnect(duckdb::duckdb(), ":memory:")
 
 # write a data.frame to the database
-dbWriteTable(con, paste(from_df), get(from_df))
+DBI::dbWriteTable(con, paste(from_df), get(from_df))
 
 # execute required SQL query against the new DuckDB table
-statement_result <- dbGetQuery(con, query)
+statement_result <- DBI::dbGetQuery(con, query)
 
 # close the connection
-dbDisconnect(con, shutdown = TRUE)
+DBI::dbDisconnect(con, shutdown = TRUE)
 
 # return results
 return(statement_result)
--- R/duckdf_persist.R
+++ R/duckdf_persist.R
@@ -1,22 +1,22 @@
 duckdf_persist <- function(query = ""){
 
   # Extract dataframe name using sting splits
-  query_split <- stri_split_fixed(query, "FROM", omit_empty = TRUE)
+  query_split <- stringi::stri_split_fixed(query, "FROM", omit_empty = TRUE)
   query_split <- trimws(unlist(query_split))
 
-  from_df <- stri_extract_first_words(query_split[2], locale = NULL)
+  from_df <- stringi::stri_extract_first_words(query_split[2], locale = NULL)
 
   # If the database exists, just execute the query
   if(file.exists(paste(from_df)) == TRUE) {
 
     # open db connection
-    con <- dbConnect(duckdb::duckdb(), paste(from_df))
+    con <- DBI::dbConnect(duckdb::duckdb(), paste(from_df))
 
     # execute required SQL query against the existing DuckDB table
-    statement_result <- dbGetQuery(con, query)
+    statement_result <- DBI::dbGetQuery(con, query)
 
     # close the connection
-    dbDisconnect(con, shutdown = TRUE)
+    DBI::dbDisconnect(con, shutdown = TRUE)
 
     # return results
     return(statement_result)
@@ -25,16 +25,16 @@ duckdf_persist <- function(query = ""){
   } else
   {
     # open db connection
-    con <- dbConnect(duckdb::duckdb(), paste(from_df))
+    con <- DBI::dbConnect(duckdb::duckdb(), paste(from_df))
 
     # write a data.frame to the database
-    dbWriteTable(con, paste(from_df), get(from_df))
+    DBI::dbWriteTable(con, paste(from_df), get(from_df))
 
     # execute required SQL query against the new DuckDB table
-    statement_result<- dbGetQuery(con, query)
+    statement_result<- DBI::dbGetQuery(con, query)
 
     # close the connection
-    dbDisconnect(con, shutdown = TRUE)
+    DBI::dbDisconnect(con, shutdown = TRUE)
 
     # return results
     return(statement_result)

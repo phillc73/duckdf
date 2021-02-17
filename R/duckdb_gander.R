8d6d18c40ed2fe46ecfd35dd00b59b0979a62fa2
Phill Clarke <phill@starkingdom.co.uk>
2021-02-17 22:43:28 +0100

    Add duckdf_gander() to inspect duckdbs

---
+++ R/duckdb_gander.R
@@ -0,0 +1,71 @@
+duckdb_gander <- function(db_name,
+                           show_types = FALSE) {
+
+    if (file.exists(db_name) == FALSE) {
+
+    # Display error message if no table exists by that name
+    gander_result <-
+    "No database exists with that name in the current directory"
+
+    return(gander_result)
+
+    } else {
+
+    # Open a databse connection
+    con <- DBI::dbConnect(duckdb::duckdb(), db_name)
+
+    # Return the database metadata
+    gander_result <- DBI::dbListTables(con, db_name)
+
+    # Close the connection
+    DBI::dbDisconnect(con, shutdown = TRUE)
+
+    # Check to see if there are two tables
+    if (exists(stringi::stri_extract_first_words(gander_result[2])) == TRUE) {
+
+    # Make a list containing both tables
+    gander_result_tables <- list(
+        duckdf_persist(paste0("SELECT * from ",
+                       stringi::stri_extract_first_words(gander_result[1],
+                       locale = NULL), " LIMIT 5;")),
+
+        duckdf_persist(paste0("SELECT * from ",
+                       stringi::stri_extract_first_words(gander_result[2],
+                       locale = NULL), " LIMIT 5;"),
+                       db_name = stringi::stri_extract_first_words(gander_result[1]))
+                       )
+
+    # Give each list element a name
+    names(gander_result_tables) <-
+    c(paste(stringi::stri_extract_first_words(gander_result[1])),
+    paste(stringi::stri_extract_first_words(gander_result[2])))
+
+    } else {
+
+    # If there's only one table, just make a one item list
+    gander_result_tables <- list(
+        duckdf_persist(paste0("SELECT * from ",
+                       stringi::stri_extract_first_words(gander_result[1],
+                       locale = NULL), " LIMIT 5;"))
+                       )
+
+    # Give each list element a name
+    names(gander_result_tables) <-
+    c(paste(stringi::stri_extract_first_words(gander_result[1])))
+
+    }
+
+    if (show_types == TRUE) {
+
+    # return results showing element types
+    return(str(gander_result_tables))
+
+    } else {
+
+    # return results with showing each element type
+    return(gander_result_tables)
+    }
+
+    }
+
+}

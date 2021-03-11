171081c300675e5b40c604a7bcdec87875bd547a
Phill Clarke <phill@starkingdom.co.uk>
2021-03-11 20:26:33 +0100

    Add documentation for ingest()

--- R/ingest.R
+++ R/ingest.R
@@ -1,3 +1,85 @@
+#' Ingest data
+#'
+#' \code{ingest} loads a CSV file from the current working directory and
+#'   returns either a new data.frame, data.table, tibble or an on-disk
+#'   duckdb.
+#'
+#' @param name String. The name of the new data.frame. data.table, tibble
+#'   or on-disk duckdb. Required.
+#'
+#' @param files String. The name of the CSV file to ingest. File must be
+#'   located in the current working directory. Required.
+#'
+#' @param persist Boolean. If TRUE and new duckdb is written to disk.
+#'   If FALSE by default a data.frame is returned. Default TRUE.
+#'
+#' @param object_type String. The type of object to return. Default
+#'   is a data.frame. Both data.table and tibble are supported. Only
+#'   relevant if \code{persist] is set to FALSE.
+#'
+#' @param header Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @param na.strings Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @param nrow.check Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @param delim Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @param quote Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @param col.names Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @param lower.case.names Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @param sep Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @param transaction Parameter to support all duckdb options. See duckdb
+#'   documentation for \code{duckdb_read_csv}.
+#'   \url{https://cran.r-project.org/web/packages/duckdb/index.html}
+#'
+#' @return If a CSV file with corresponding name is found in the current
+#'   working directory, this function ingests that file, returning either
+#'   a data.frame, data.table, tibble or on-disk duckdb.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Ingest the filename.csv file as an on-disk duckdb
+#' # database named `descriptive_name`
+#'
+#' duckdf::ingest(name = "descriptive_name",
+#'               file = "filename.csv",
+#'               persist = TRUE)
+#'
+#' # Ingest the filename.csv file, as a data.table
+#' # named `descriptive_name`
+#'
+#' duckdf::ingest(name = "descriptive_name",
+#'               file = "filename.csv",
+#'               persist = FALSE,
+#'               object_type = "data.table")
+#'
+#'  }
+#'
+#' @export
+#'
+
 ingest <- function(name,
                   files,
                   persist = TRUE,

1953c36f2e7aab86c73bcc7abeac51834f9750ad
Phill Clarke <phill@starkingdom.co.uk>
2021-03-18 23:13:50 +0100

    Working on hatch()

--- R/hatch.R
+++ R/hatch.R
@@ -1,71 +1,33 @@
-hatch <- function(query = "",
-                           db_name = NULL) {
+hatch <- function(query = "") {
 
-  # Extract dataframe name using sting splits
-  query_split <- stringi::stri_split_fixed(query, "INTO",
-                                           omit_empty = TRUE,
-                                           opts_fixed = stringi::stri_opts_fixed(case_insensitive = TRUE))
+# Extract dataframe name using string splits
+query_split <- stringi::stri_split_fixed(query, "INTO",
+                                         omit_empty = TRUE,
+                                         opts_fixed = stringi::stri_opts_fixed(case_insensitive = TRUE))
 
-  query_split <- trimws(unlist(query_split))
+query_split <- trimws(unlist(query_split))
 
-  to_df <- stringi::stri_extract_first_words(query_split[2], locale = NULL)
+from_df <- stringi::stri_extract_first_words(query_split[2], 
+                                                   locale = NULL)
 
-  # If a JOIN exists do this
-if (stringi::stri_detect_fixed(query, "JOIN",
-                  opts_fixed = stringi::stri_opts_fixed(case_insensitive = TRUE)) == TRUE) {
+query_split_values <- stringi::stri_split_fixed(query_split[2], "VALUES (",
+                                         omit_empty = TRUE,
+                                         opts_fixed = stringi::stri_opts_fixed(case_insensitive = TRUE))
 
-# Extract dataframe name using sting splits
-query_split_second <- stringi::stri_split_fixed(query, "JOIN",
-                                                omit_empty = TRUE,
-                                                opts_fixed = stringi::stri_opts_fixed(case_insensitive = TRUE))
+query_split_values <- trimws(unlist(query_split_values))
 
-query_split_second <- trimws(unlist(query_split_second))
+query_split_values <- stringi::stri_split_fixed(query_split_values[2], ")",
+                                         omit_empty = TRUE,
+                                         opts_fixed = stringi::stri_opts_fixed(case_insensitive = TRUE))
 
-from_df_second <- stringi::stri_extract_first_words(query_split_second[2],
-                                                    locale = NULL)
-                  }
+query_split_values <- trimws(unlist(query_split_values))
 
-# check for a user set db_name, otherwise use first dataframe name
-if (is.null(db_name)) {
-  db_name <- to_df
+new_df <- get(from_df)
+
+new_values <- as.list(scan(text = query_split_values, what = "", sep = ","))
+
+new_df[nrow(new_df) + 1,] <- new_values
+
+assign(paste(from_df), new_df)
+
 }
-
-  # If the database exists, just execute the query
-  if (file.exists(db_name) == TRUE) {
-
-    # open db connection
-    con <- DBI::dbConnect(duckdb::duckdb(), db_name)
-
-    # execute required SQL query against the existing DuckDB table
-    statement_result <- DBI::dbGetQuery(con, query)
-
-    # close the connection
-    DBI::dbDisconnect(con, shutdown = TRUE)
-
-    # return results
-    return(statement_result)
-
-    # if the db doesn't exist, create it and then execute the query
-  } else {
-
-    # open db connection
-    con <- DBI::dbConnect(duckdb::duckdb(), db_name)
-
-    # write a data.frame to the database
-    DBI::dbWriteTable(con, paste(from_df), get(from_df))
-
-    if (exists("from_df_second") == TRUE) {
-      # register a second table in duckdb if a second table is required
-      DBI::dbWriteTable(con, paste(from_df_second), get(from_df_second))
-      }
-    }
-
-    # execute required SQL query against the new DuckDB table
-    statement_result <- DBI::dbGetQuery(con, query)
-
-    # close the connection
-    DBI::dbDisconnect(con, shutdown = TRUE)
-
-    # return results
-    return(statement_result)
-  }

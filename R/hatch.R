dd985f8bbac5f2b270785380a2f0df3bf4f4e3e4
Phill Clarke <phill@starkingdom.co.uk>
2021-03-19 19:44:56 +0100

    Finalise hatch() function

--- R/hatch.R
+++ R/hatch.R
@@ -22,12 +22,19 @@ query_split_values <- stringi::stri_split_fixed(query_split_values[2], ")",
 
 query_split_values <- trimws(unlist(query_split_values))
 
+# Put the dataframe from the query into a new object
 new_df <- get(from_df)
 
+# put the values from the query into a list
 new_values <- as.list(scan(text = query_split_values, what = "", sep = ","))
 
+# add a new row to the new dataframe
 new_df[nrow(new_df) + 1,] <- new_values
 
-assign(paste(from_df), new_df)
+# assign the new dataframe to the dataframe from the query
+result_statement <- assign(paste(from_df), new_df)
+
+# return the result
+return(result_statement)
 
 }
